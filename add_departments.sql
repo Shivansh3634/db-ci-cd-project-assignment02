532f7b2db8a882c3fb04a6956b3853a3832561d9
Shivansh3634 <skumar3634@conestogac.on.ca>
2025-06-07 19:16:33 -0400

    Testing CI/CD trigger by editing SQL file

---
+++ add_departments.sql
@@ -0,0 +1,7 @@
+ Shivansh (8983634) for testing CI/CD
+
+CREATE TABLE IF NOT EXISTS departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100),
+    location VARCHAR(100)
+);
